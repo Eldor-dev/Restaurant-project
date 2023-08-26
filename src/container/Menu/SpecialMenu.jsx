cc06ec2078ba85a6eaf22d2079a33418042d932f
Eldor Azimov <eldorazimov37@gmail.com>
2023-08-26 23:11:30 +0900

    Restaurant project

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -10,12 +10,18 @@ const SpecialMenu = () => (
       <SubHeading title="Menu that fits you palatte" />
       <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
+
     <div className="app__specialMenu-menu">
       <div className="app__specialMenu-menu_wine flex__center">
         <p className="app__specialMenu-menu_heading">Wine & Beer</p>
         <div className="app__specialMenu-menu_items">
           {data.wines.map((wine, index) => (
-            <p>{wine.title}</p>
+            <MenuItem
+              key={wine.title + index}
+              title={wine.title}
+              price={wine.price}
+              tags={wine.tags}
+            />
           ))}
         </div>
       </div>
@@ -28,10 +34,21 @@ const SpecialMenu = () => (
         <p className="app__specialMenu-menu_heading">Wine & Beer</p>
         <div className="app__specialMenu-menu_items">
           {data.cocktails.map((cocktail, index) => (
-            <p>{cocktail.title}</p>
+            <MenuItem
+              key={cocktail.title + index}
+              title={cocktail.title}
+              price={cocktail.price}
+              tags={cocktail.tags}
+            />
           ))}
         </div>
       </div>
+    </div>
+
+    <div style={{ marginTop: "15px" }}>
+      <button type="button" className="custom__button">
+        View More
+      </button>
     </div>
   </div>
 );
